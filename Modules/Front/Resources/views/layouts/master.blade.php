fff5230d5281f9a70561275ece1ff04bc8d4222d
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2022-07-01 00:02:37 +0100

    Add twitter tag

--- Modules/Front/Resources/views/layouts/master.blade.php
+++ Modules/Front/Resources/views/layouts/master.blade.php
@@ -8,6 +8,19 @@ use Modules\Front\Internal\User;
     <title><?=$title ?? 'ABRouter - AB-tests on backend service'?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <?php if (env('MARKETING_ENABLED') === 'enabled'): ?>
+    <!-- Twitter universal website tag code -->
+    <script>
+        !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+        },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+        // Insert Twitter Pixel ID and Standard Event data below
+        twq('init','o9awu');
+        twq('track','PageView');
+    </script>
+    <!-- End Twitter universal website tag code -->
+    <?php endif;?>
+
     <script>
         window['_fs_debug'] = false;
         window['_fs_host'] = 'fullstory.com';
