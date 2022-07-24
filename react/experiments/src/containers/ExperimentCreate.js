5503fc425610f11aef5937931795ab7bfb633ae7
omelyko <sekret.krut@gmail.com>
2022-07-24 23:35:46 +0300

    Fix

--- react/experiments/src/containers/ExperimentCreate.js
+++ react/experiments/src/containers/ExperimentCreate.js
@@ -35,11 +35,6 @@ class ExperimentCreate extends React.Component {
         this.props.parent.appState.adding=true;
         this.forceUpdate();
 
-        if (window.mode === 'feature-toggle' && this.props.parent.appState.mode !== 'edit') {
-            this.createFeatureToggle()
-        }
-
-        event.preventDefault();
         saveExperimentState(this.props.parent.appState.activeItem).then(response => {
             this.props.parent.experimentList.load();
             this.showNotifications();
@@ -178,6 +173,7 @@ class ExperimentCreate extends React.Component {
 
     changeBranchName(e) {
         this.props.parent.changeBranchName(e);
+        this.forceUpdate();
     }
 
     changeBranchUid(e) {
